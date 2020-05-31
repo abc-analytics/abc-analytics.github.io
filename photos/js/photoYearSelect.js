8abd358a4cc6aa84e87e4a0b51cb8493c691ca70
ABC <abc-analytics@outlook.com>
2020-05-31 14:26:14 -0400

    Delete photoYearSelect.js

--- photos/js/photoYearSelect.js
+++
@@ -1,9 +0,0 @@
-
-  $("#dropdown-menu").change(function() {
-
-    $("#photo-showcase-container").empty();
-    var filePath = "html/"+$(this).val()+".html";
-    alert("this file to get is "+filePath);
-    $("#photo-showcase-container").load(filePath);
-  });
-
