9708f16adcacf71da7ee73fff0fb1b7e0c457bf4
ABC <abc-analytics@outlook.com>
2020-05-31 13:39:35 -0400

    Add files via upload

---
+++ photos/js/photoYearSelect.js
@@ -0,0 +1,11 @@
+
+$(function() {
+
+  $("dropdown-menu").change(function() {
+
+    $("#photo-showcase-container").empty();
+    var filePath = "html/"+$(this).val()+".html";
+    alert("this file to get is "+filePath);
+    $("#photo-showcase-container").load(filePath);
+  });
+});
