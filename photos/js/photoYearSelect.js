afd700f838a1725c77620296791ea965ed39eb7c
ABC <abc-analytics@outlook.com>
2020-06-01 01:48:57 -0400

    Add files via upload

--- photos/js/photoYearSelect.js
+++ photos/js/photoYearSelect.js
@@ -1,12 +1,13 @@
 
 $(document).ready(function() {
 
-$("#dropdown-menu").change(function() {
+  $("#dropdown-menu").change(function() {
 
-  $("#photo-showcase-container").empty();
-  var filePath = "html/"+$(this).val()+".html";
-  $("#photo-showcase-container").load(filePath);
-});
+    $("#photo-showcase-container").empty();
+    var filePath = "html/"+$(this).val()+".html"; //string of the path to the relevant year html file
+    $("#photo-showcase-container").load(filePath); //load that html file in the showcase area
+    
+  });
 
 });
 
