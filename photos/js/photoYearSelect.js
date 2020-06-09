14a99abd285aeda73069fe2f30924d5d3deba175
ABC <abc-analytics@outlook.com>
2020-06-09 15:00:19 -0400

    photo update

--- photos/js/photoYearSelect.js
+++ photos/js/photoYearSelect.js
@@ -4,8 +4,7 @@ $(document).ready(function() {
   $("#dropdown-menu").change(function() {
     
     var filePath = "html/"+$(this).val()+".html"; //string of the path to the relevant year html file
-    $("#photo-showcase-container").load(filePath); //load that html file in the showcase area
-    setTimeout(function(){photoTransformScript()}, 500);
+    $("#photo-showcase-container").load(filePath, photoTransform); //load that html file in the showcase area, with a randomization function as callback
     
   });
 
