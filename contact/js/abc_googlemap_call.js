518c5ec9d4dfae95beb9c30256bb04d40680ce7d
ABC <abc-analytics@outlook.com>
2020-07-14 04:36:53 -0400

    update

--- contact/js/abc_googlemap_call.js
+++ contact/js/abc_googlemap_call.js
@@ -11,7 +11,7 @@ function initMap() {
     })
 
     var marker = new google.maps.Marker({
-        position: new google.maps.LatLng(-54.2815, -36.5085),
+        position: new google.maps.LatLng(55.9533, 3.1883), //the 
         icon: "https://i.ibb.co/HNYVYjM/map-cursor.png",
         map: map,
         title: "Applied Bayesian Capital",
